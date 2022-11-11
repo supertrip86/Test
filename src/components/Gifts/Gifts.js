4956c513cef07ee14fb048bea519d5ddc2ed552f
Giovanni Giunta <gio.giunta.86@gmail.com>
2022-11-11 10:41:55 +0100

    Primo deploy

--- src/components/Gifts/Gifts.js
+++ src/components/Gifts/Gifts.js
@@ -1,10 +1,5 @@
 import './Gifts.css';
 import Logo1 from '../../assets/images/Gifts/1.jpg';
-import Logo2 from '../../assets/images/Gifts/2.jpg';
-import Logo3 from '../../assets/images/Gifts/3.jpg';
-import Logo4 from '../../assets/images/Gifts/4.jpg';
-import Logo5 from '../../assets/images/Gifts/5.jpg';
-import Logo6 from '../../assets/images/Gifts/6.jpg';
 
 const Gifts = () => {
     return (
@@ -18,22 +13,7 @@ const Gifts = () => {
                     </div>
                     <div className="col-md-9 owl-carousel owl-theme">
                         <div className="client-logo">
-                            <a href="https://duruthemes.com/demo/html/olivia-enrico/demo1/index.html"><img src={Logo1} alt="" /></a>
-                        </div>
-                        <div className="client-logo">
-                            <a href="https://duruthemes.com/demo/html/olivia-enrico/demo1/index.html"><img src={Logo2} alt="" /></a>
-                        </div>
-                        <div className="client-logo">
-                            <a href="https://duruthemes.com/demo/html/olivia-enrico/demo1/index.html"><img src={Logo3} alt="" /></a>
-                        </div>
-                        <div className="client-logo">
-                            <a href="https://duruthemes.com/demo/html/olivia-enrico/demo1/index.html"><img src={Logo4} alt="" /></a>
-                        </div>
-                        <div className="client-logo">
-                            <a href="https://duruthemes.com/demo/html/olivia-enrico/demo1/index.html"><img src={Logo5} alt="" /></a>
-                        </div>
-                        <div className="client-logo">
-                            <a href="https://duruthemes.com/demo/html/olivia-enrico/demo1/index.html"><img src={Logo6} alt="" /></a>
+                            <a href="/"><img src={Logo1} alt="" /></a>
                         </div>
                     </div>
                 </div>
